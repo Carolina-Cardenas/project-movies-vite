907753251c19e88631338dd1eb7937ea16f5922e
Carolina-Cardenas <carolina.beatriz.cardenas@gmail.com>
2023-11-07 23:07:12 +0100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,25 @@
+
+import { BrowserRouter } from "react-router-dom"
+
+import { NavBar } from './components/NavBar';
+import { routes } from './components/routes';
+
 export const App = () => {
-  return <div>Find me in src/app.jsx!</div>;
-};
+return (
+  <BrowserRouter>
+  <NavBar/>
+  {routes()}
+  </BrowserRouter>
+)
+}
+
+
+  //   <Router>
+//     <Route path="/" exact>
+//       <Movies />
+//     </Route>
+//     <Route path="/movies/:id">
+//       <Detail />
+//     </Route>
+//   </Router>
+ 
